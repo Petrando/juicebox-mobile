64ec86fb357be80cb2180d5cbd98f1f0839f6574
Petrando <petrandorichard@gmail.com>
2024-12-01 16:38:18 +0800

    Button UI logic based on homeState

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import { TouchableOpacity, View, Text, StyleSheet } from 'react-native';
+import Welcome from './pages/home/welcome/Welcome';
 
 const Button = ({ homeState, setHomeState }) => {
     const atSwipe = homeState.startsWith("swipe")
+    const atDataInput = homeState.startsWith("data-input")
     const buttonLabel = homeState === "welcome"?"Get a reality check":
         homeState === "swipe3"?"Continue":"Get started"
 
+    const buttonStyle = homeState === "welcome"?styles.welcomeButton:
+        (homeState === "swipe3" || atDataInput)?styles.whiteButton:
+            styles.darkButton
+
+    const buttonTextStyle = homeState === "welcome"?styles.welcomeText:
+        (homeState === "swipe3" || atDataInput)?styles.whiteButtonText:
+            styles.darkButtonText
+    
     return (
         <View style={styles.container}>
-            <TouchableOpacity style={styles.button} 
+            <TouchableOpacity style={[styles.button, buttonStyle]} 
                 onPress={()=>{
                     if(homeState === "welcome"){
                       setHomeState("swipe1")
@@ -16,7 +26,7 @@ const Button = ({ homeState, setHomeState }) => {
                       setHomeState("data-input")
                     }        
                   }}>
-                <Text style={styles.text}>{buttonLabel}</Text>
+                <Text style={[styles.text, buttonTextStyle]}>{buttonLabel}</Text>
             </TouchableOpacity>
         </View>
     )
@@ -31,7 +41,7 @@ const styles = StyleSheet.create({
         width: '90%',
         height: 60,
         borderRadius: 19,
-        backgroundColor: '#CDAAFF',
+        
         display: 'flex', justifyContent: 'center', alignItems: 'center'
     },
     text: {
@@ -40,7 +50,25 @@ const styles = StyleSheet.create({
         fontWeight: 400,
         lineHeight: 21.6,
         letterSpacing: 0.02,
-        textAlign: 'center',
+        textAlign: 'center',        
+    },
+    welcomeButton: {
+        backgroundColor: '#CDAAFF',
+    },
+    welcomeText: {
+        color: '#0C0D10'
+    },
+    darkButton:{
+        backgroundColor: '#0C0D10',
+        borderColor: '#FAFAFA', borderWidth: 1, borderStyle: 'solid'
+    },
+    darkButtonText: {
+        color: '#FAFAFA'
+    },
+    whiteButton: {
+        backgroundColor: '#FAFAFA'
+    },
+    whiteButtonText: {
         color: '#0C0D10'
     }
 })
